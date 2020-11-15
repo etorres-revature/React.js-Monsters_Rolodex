499cab5fb87eeca9f41693d9146776acd64a5f6f
Eric D. Torres <etorresnotary@gmail.com>
2020-11-14 19:27:05 -0600

    playing around with app

--- monsters_rolodex/src/App.js
+++ monsters_rolodex/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: "Frankenstein",
+          id: "mon1",
+        },
+        {
+          name: "Dracula",
+          id: "mon2",
+        },
+        {
+          name: "Zombie",
+          id: "mon3",
+        },
+        {
+          name: "Mummy",
+          id: "mon4",
+        },
+        {
+          name: "Werewolf",
+          id: "mon5",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}> {monster.name} </h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
