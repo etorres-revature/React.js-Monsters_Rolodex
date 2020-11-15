3b0ef7f539dbd2f94ab6080c3e6ae16071e48c4b
Eric D. Torres <etorresnotary@gmail.com>
2020-11-14 20:34:56 -0600

    adding email addresses for monsters

--- monsters_rolodex/src/components/card/card.component.jsx
+++ monsters_rolodex/src/components/card/card.component.jsx
@@ -9,7 +9,8 @@ export const Card = (props) => {
         alt="monster"
         src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
       />
-      <h1>{props.monster.name}</h1>
+      <h2>{props.monster.name}</h2>
+      <p>{props.monster.email}</p>
     </div>
   );
 };
