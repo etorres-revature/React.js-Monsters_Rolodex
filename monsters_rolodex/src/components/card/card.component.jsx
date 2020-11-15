b6b0fbdf020f2e9bec7dee69f0a2c8a10cfece1b
Eric D. Torres <etorresnotary@gmail.com>
2020-11-14 20:32:32 -0600

    monster pictures

--- monsters_rolodex/src/components/card/card.component.jsx
+++ monsters_rolodex/src/components/card/card.component.jsx
@@ -5,6 +5,10 @@ export const Card = (props) => {
   return (
     <div className="card-container">
       {" "}
+      <img
+        alt="monster"
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+      />
       <h1>{props.monster.name}</h1>
     </div>
   );
